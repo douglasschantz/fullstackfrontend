1efe53c4a802f99a72acd01c188697861b5d3a23
douglasschantz <schantzdouglas@gmail.com>
2023-08-17 15:14:48 -0300

    Folder Structure for React app

---
+++ src/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">FullStack Application</a>
+
+                    <button class="btn btn-light" type="submit">Add User</button>
+                    
+
+
+                </div>
+            </nav>
+        </div>
+    )
+}
