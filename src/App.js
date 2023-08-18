f2b6a53411eb6f92d494a3fac3ebd400787e550d
douglasschantz <schantzdouglas@gmail.com>
2023-08-18 11:21:02 -0300

    Create Homepage using Bootstrap table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './layout/Navbar';
+import Home from './page/Home';
 
 function App() {
   return (
     <div className="App">
       
       <Navbar/>
+      <Home/>
 
     </div>
   );
